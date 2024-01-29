4309fbbaace40753e6109c9f961b9af13bab5729
IshaanData <idatta@student.ubc.ca>
2024-01-28 17:46:48 -0800

    comments

--- 23-I-12_SysArch/Experiments/ishaan_workspace/src/pipeline/node_test/node_test/test.py
+++ 23-I-12_SysArch/Experiments/ishaan_workspace/src/pipeline/node_test/node_test/test.py
@@ -11,10 +11,15 @@ print(type(image_data[0]))
 # add embedded testing methods
 # on surface: echo "source install/setup.bash" >> ~/.bashrc
 # pipeline for metric evaluation
-# launch file
 # logging metrics somewhere
 # use open CV pipleine w/ bridge
 # simple yolo bounding + launch window
-# peek next architecture type
 # node architecture testing via launch file
 # record highest value in header
+# !!! rgbd camera considerations...
+# make zed camrea feed publisher + start tet yolo application + viewer window -> how test acc?
+# 3rd topic research after finished openCV + dashboard + more ROS tutorials
+
+# max cpu usage, initialized variable + comparison, average using metrics later, how to find process IDs
+# colcon test to test packages just built
+# ROS tutorials go hard after finishing zed camera interfacing
