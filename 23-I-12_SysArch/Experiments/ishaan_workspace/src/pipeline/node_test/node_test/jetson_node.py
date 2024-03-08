8fbe8e7b9d13413438db34509f49aa0b1840589d
Ishaan Datta <Ishaandatta737@gmail.com>
2024-03-08 14:01:20 -0800

    trying display window

--- 23-I-12_SysArch/Experiments/ishaan_workspace/src/pipeline/node_test/node_test/jetson_node.py
+++ 23-I-12_SysArch/Experiments/ishaan_workspace/src/pipeline/node_test/node_test/jetson_node.py
@@ -38,12 +38,13 @@ class JetsonNode(Node):
         # self.get_logger().info(f"Latency of {msg.header.frame_id} is {(self.get_clock().now().nanoseconds() - msg.header.stamp.nanoseconds()) / 1e6} milliseconds")
         
         cv_image  = self.bridge.imgmsg_to_cv2(msg)
+        cv2.imshow("Image window", cv_image)
         height, width, channels = cv_image.shape
         print(height, width, channels)
         sized_image = cv2.resize(cv_image, (640, 480))
         image = cv2.cvtColor(sized_image, cv2.COLOR_BGR2RGB)
         # cv2.imread(cv_image, cv2.IMREAD_COLOR)
-        cv2.imshow("Image window", image)
+        
         # self.detection(image)
         
         # self.latency, self.frame_id, self.frames = latency.nanoseconds / 1e6, msg.header.frame_id, self.frames + 1
