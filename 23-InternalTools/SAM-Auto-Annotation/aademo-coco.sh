b02e69bb0a1958e2dc7eee6958b9aefc686824da
Mujtaba Ali <mujtaba.ali.syed.m.a.s@gmail.com>
2024-03-09 00:20:03 -0800

    Create aademo-coco.sh

---
+++ 23-InternalTools/SAM-Auto-Annotation/aademo-coco.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=00:20:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=16GB
+#SBATCH --job-name=aa_test
+#SBATCH --account=st-sielmann-1-gpu
+#SBATCH --mail-user=<your-email>
+#SBATCH --mail-type=ALL
+#SBATCH --output=<path-to-Working-Directory>/out.txt
+#SBATCH --error=<path-to-Working-Directory>/error.txt
+
+############################################################################################################################
+
+python ttsplit.py test-images train test 0.8
+python autoannot.py train annotations true
+python cocogen.py train/annotations
