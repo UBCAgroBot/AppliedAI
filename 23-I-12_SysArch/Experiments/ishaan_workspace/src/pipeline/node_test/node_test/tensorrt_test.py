3c7e2a102d2c8a0ff6f2d01de083be9d8b5348e1
Ishaan Datta <Ishaandatta737@gmail.com>
2024-03-14 19:27:00 -0700

    tensorrt test script

---
+++ 23-I-12_SysArch/Experiments/ishaan_workspace/src/pipeline/node_test/node_test/tensorrt_test.py
@@ -0,0 +1,38 @@
+from ultralytics import YOLO
+import cv2
+import pyzed.sl as sl
+
+# from models.pycuda_api import TRTEngine
+
+def main():
+    tensorrt_model = YOLO('yolov8x_onnx.engine')
+
+    init = sl.InitParameters()
+    cam = sl.Camera()
+    if not cam.is_opened():
+        print("Opening ZED Camera...")
+    status = cam.open(init)
+    if status != sl.ERROR_CODE.SUCCESS:
+        print(repr(status))
+        exit()
+
+    runtime = sl.RuntimeParameters()
+    mat = sl.Mat()
+
+    key = ''
+    while key != 113:  # for 'q' key
+        err = cam.grab(runtime)
+        if err == sl.ERROR_CODE.SUCCESS:
+            cam.retrieve_image(mat, sl.VIEW.LEFT)
+            # Convert the image to a numpy array
+            image_np = mat.get_data()
+            results = tensorrt_model(image_np)
+            results.show()
+            # cv2.imshow("ZED", gray)
+            key = cv2.waitKey(5)
+        else:
+            key = cv2.waitKey(5)
+    cv2.destroyAllWindows()
+    cam.close()
+
+main()
