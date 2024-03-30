e87d62f82d5cd7055c05167ea246d9b0b49d3c90
Agrobot-Applied-AI <agrobotappliedai@gmail.com>
2024-03-30 17:55:53 +0000

    idk old lol

--- 23-I-12_SysArch/Experiments/ishaan_workspace/src/pipeline/node_test/node_test/tensorrt_test.py
+++ 23-I-12_SysArch/Experiments/ishaan_workspace/src/pipeline/node_test/node_test/tensorrt_test.py
@@ -15,7 +15,7 @@ while vidObj.isOpened():
     
     if success:
         # Run YOLOv8 inference on the frame
-        results = tensorrt_model(frame, stream=True) # set stream to false
+        results = tensorrt_model(frame, stream=False) # set stream to false
         # for result in results:
             # annotated_frame = result[0].plot()
             # print(result[0].speed)
@@ -27,15 +27,15 @@ while vidObj.isOpened():
             # pass
 
         # Visualize the results on the frame
-        annotated_frame = results[0].plot()
-        print(results[0].speed)
+        # annotated_frame = results[0].plot()
+        # print(results[0].speed)
         # print(repr(result[0]))
         # print(repr(result[1]))
         # Display the annotated frame
-        cv2.imshow("YOLOv8 Inference", annotated_frame)
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+        # cv2.imshow("YOLOv8 Inference", annotated_frame)
+        # # Break the loop if 'q' is pressed
+        # if cv2.waitKey(1) & 0xFF == ord("q"):
+        #     break
     else:
         # Break the loop if the end of the video is reached
         break
